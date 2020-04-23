159ba6e4690273760fac5e5fb86ee5bf79f44a5b
赵瑞 <zhaorui@mininglamp.com>
2020-04-23 16:32:00 +0800

    新增javaassist动态代理

--- src/test/java/JavassistProxyFactory02.java
+++ src/test/java/JavassistProxyFactory02.java
@@ -1,23 +1,22 @@
 import javassist.util.proxy.MethodHandler;
+import javassist.util.proxy.Proxy;
 import javassist.util.proxy.ProxyFactory;
-
-import java.lang.reflect.Method;
 
 public class JavassistProxyFactory02 {
     public Object getProxy(Class clazz) throws InstantiationException, IllegalAccessException {
-        ProxyFactory proxyFactory = new ProxyFactory();
-        proxyFactory.setSuperclass(clazz);
-        proxyFactory.setHandler(new MethodHandler() {
-            public Object invoke(Object self, Method thismethod, Method proceed, Object[] args) throws Throwable {
-                System.out.println("--------------------------------");
-                System.out.println(self.getClass());
-                System.out.println("代理类对方法的代理引用:" + thismethod.getName());
-                System.out.println("开启事务 -------");
-                Object result = proceed.invoke(self, args);
-                System.out.println("提交事务 -------");
-                return result;
-            }
+        ProxyFactory f = new ProxyFactory();
+        f.setSuperclass(clazz);
+        f.setFilter(m -> {
+            // ignore finalize()
+            return !m.getName().equals("finalize");
         });
-        return proxyFactory.createClass().newInstance();
+        Class c = f.createClass();
+        MethodHandler mi = (self, m, proceed, args) -> {
+            System.out.println("Name: " + m.getName());
+            return proceed.invoke(self, args);  // execute the original method.
+        };
+        Object foo = c.newInstance();
+        ((Proxy) foo).setHandler(mi);
+        return foo;
     }
 }
